5e067cccf390250cb9eba15083b0a4686f77bdd1
Werner Strijewski <Werner_Strijewski@yamaha-motor.ca>
2024-11-17 19:30:52 -0500

    3 new problems, updated

--- JavaProblems/src/org/strijewski/problems/checkPermutations/CheckPermutation.java
+++ JavaProblems/src/org/strijewski/problems/checkPermutations/CheckPermutation.java
@@ -1,2 +1,33 @@
-package org.strijewski.problems.checkPermutations;public class CheckPermutation {
+package org.strijewski.problems.checkPermutations;
+
+import java.util.Arrays;
+
+public class CheckPermutation {
+
+    // Method to check if two strings are permutations of each other
+    public static boolean arePermutations(String str1, String str2) {
+        // If the lengths are not the same, they can't be permutations
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Convert strings to character arrays and sort them
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        // Compare sorted arrays
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+        String str3 = "hello";
+
+        System.out.println("\"" + str1 + "\" and \"" + str2 + "\" are permutations? " + arePermutations(str1, str2)); // true
+        System.out.println("\"" + str1 + "\" and \"" + str3 + "\" are permutations? " + arePermutations(str1, str3)); // false
+    }
 }
--- JavaProblems/src/org/strijewski/problems/isUnique/UniqueCharacters.java
+++ JavaProblems/src/org/strijewski/problems/isUnique/UniqueCharacters.java
@@ -1,2 +1,32 @@
-package org.strijewski.problems.isUnique;public class UniqueCharacters {
+package org.strijewski.problems.isUnique;
+
+import java.util.HashSet;
+
+public class UniqueCharacters {
+
+    // Method to check if a string has all unique characters
+    public static boolean hasUniqueCharacters(String str) {
+        // Use a HashSet to track seen characters
+        HashSet<Character> seenCharacters = new HashSet<>();
+
+        for (char c : str.toCharArray()) {
+            // If the character is already in the set, it's not unique
+            if (seenCharacters.contains(c)) {
+                return false;
+            }
+            seenCharacters.add(c);
+        }
+
+        return true; // All characters are unique
+    }
+
+    public static void main(String[] args) {
+        String input1 = "abcdef";
+        String input2 = "hello";
+        String input3 = "";
+
+        System.out.println("Does \"" + input1 + "\" have all unique characters? " + hasUniqueCharacters(input1));
+        System.out.println("Does \"" + input2 + "\" have all unique characters? " + hasUniqueCharacters(input2));
+        System.out.println("Does \"" + input3 + "\" have all unique characters? " + hasUniqueCharacters(input3));
+    }
 }
